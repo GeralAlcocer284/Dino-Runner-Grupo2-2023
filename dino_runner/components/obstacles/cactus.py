5a001bbd92befae5e02553349557be2390f61f41
Geral Brandon Alcocer Candia <brandonbpm_6039@hotmail.com>
2023-05-02 19:57:08 -0400

    Clase 4

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,6 +1,6 @@
 import random
 from dino_runner.components.obstacles.obstacle import Obstacle
-from dino_runner.utils.constants import SCREEN_HEIGHT
+from dino_runner.utils.constants import SCREEN_HEIGHT, SMALL_CACTUS, LARGE_CACTUS
 
 
 
@@ -11,9 +11,11 @@ class Cactus(Obstacle):
 
 
     def __init__(self, image):
-        self.type = random.randint(0,5)
+        self.type = random.randint(0,2)
         super().__init__(image, self.type)
-        self.rect.y = 325
-
+        if image == SMALL_CACTUS:
+            self.rect.y = 325
+        else:
+            self.rect.y = 300
    
   
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,5 +1,7 @@
 
 import pygame
+import random
+from dino_runner.components.obstacles.bird import Bird
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import BIRD, SMALL_CACTUS, LARGE_CACTUS
 
@@ -12,9 +14,14 @@ class ObstacleManager():
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS + LARGE_CACTUS))  
-
-    
+            obstacle = random.randint(0,2)
+            if obstacle == 0: 
+                self.obstacles.append(Cactus(SMALL_CACTUS))  
+            elif obstacle == 1:
+                self.obstacles.append(Cactus(LARGE_CACTUS))
+            elif obstacle == 2:
+                self.obstacles.append(Bird(BIRD))
+                
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
@@ -31,3 +38,6 @@ class ObstacleManager():
         
         for obstacle in self.obstacles:
             obstacle.draw(screen)    
+
+    def reset_obstacles(self):
+        self.obstacles = []       
